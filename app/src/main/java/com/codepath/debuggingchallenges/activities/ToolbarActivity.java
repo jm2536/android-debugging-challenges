5e76a7fedd3022148daf6617c2fea403b7a64c3e
jasmin <jm3625@humboldt.edu>
2019-07-22 15:52:27 -0700

    fixed "Toolbar"

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ToolbarActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ToolbarActivity.java
@@ -1,6 +1,7 @@
 package com.codepath.debuggingchallenges.activities;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toolbar;
